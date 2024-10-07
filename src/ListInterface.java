840f1425b8a55cb6e0c2b7dd125642c699fc8770
Waleed Awad <wawad@gmu.edu>
2024-10-07 14:14:03 -0400

    feat: Add TaskList interface for task management operations
    - Defined methods for adding, removing, retrieving, and updating tasks
    - Provides a flexible structure for future TaskList implementations (e.g., in-memory, database)

---
+++ src/ListInterface.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+
+public interface ListInterface<T> {
+	//method headings
+	
+	/**
+	 * adds an item to the list
+	 * @param item
+	 */
+	public void add(T item);
+	
+	/**
+	 * removes an item from the list if this item already
+	 * exists on the list and return true, otherwise return
+	 * false without changing the list
+	 */
+	public boolean remove(T item);
+	
+	/**
+	 * returns true if this item exists on the list
+	 * otherwise returns false
+	 * find does not change the list
+	 */
+	public boolean find(T item);
+	
+	/**
+	 * returns a string representation of the items
+	 * in the list
+	 */
+	public String toString();
+
+}
